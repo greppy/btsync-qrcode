3a64407fe7f39e89f88a5f3ecb0bc9ed7ee23dfc
Matt Okeson-Harlow <github@technomage.net>
2013-07-25 22:35:31 -0500

    Update btsync_qrcode.rb
    Added --[no-]html and --[no-]png options.
    Can now generate an HTML file of QR Codes.

--- btsync_qrcode.rb
+++ btsync_qrcode.rb
@@ -48,6 +48,15 @@ opts = OptionParser.new do |opts|
   # set defaults
   options[:file] = "~/.btsync"
   options[:size] = 300
+  options[:png] = true
+
+  opts.on( '--[no-]png', "Output as individual PNG files, default TRUE" ) do |p|
+    options[:png] = p
+  end
+
+  opts.on( '--html FILE', "Print HTML QR Codes to FILE" ) do |h|
+    options[:html] = h
+  end
 
   opts.on( '-f', '--file FILE', 'BTSync config file ( default ~/.btsync )' ) do |f|
     options[:file] = f
@@ -58,6 +67,70 @@ opts = OptionParser.new do |opts|
     options[:size] = s
   end
 end.parse!
+
+css = <<-eos
+  <style type="text/css">
+  body {
+    text-align: center;
+  }
+  table {
+    border-width: 0;
+    border-style: none;
+    border-color: #0000ff;
+    border-collapse: collapse;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+  td {
+    border-width: 0;
+    border-style: none;
+    border-color: #0000ff;
+    border-collapse: collapse;
+    padding: 0;
+    margin: 0;
+    width: 10px;
+    height: 10px;
+  }
+  td.black { background-color: #000; }
+  td.white { background-color: #fff; }
+  </style>  
+eos
+
+def html_qrcode(qr)
+  # modified from the rQRCode example at http://whomwah.github.io/rqrcode/
+  # return a string containing a QR Code HTML table
+
+  output = ""
+  output << "<table>"
+  qr.modules.each_index do |x|
+    output << "<tr>"
+    qr.modules.each_index do |y|
+      if qr.dark?(x,y)
+        output << "<td class='black'/>"
+      else
+        output << "<td class='white'/>"
+      end
+    end
+  end
+  output << "</table>"
+
+  return output
+end
+  
+html_output = ""
+
+if options[:html] then
+  html_output << <<-eos
+    <html>
+    <head>
+      <title>BTSync QR Codes</title>
+    #{css}
+    </head>
+    <body>
+  eos
+end
+
 
 # check to see if our BTSync configration file exists
 # use File.expand_path so that ~/ is interpreted
@@ -90,15 +163,38 @@ if File.file?( File.expand_path options[:file] ) then
     # get to this based on the length of the qrstring.
     qr = RQRCode::QRCode.new( qrstring, :size => 7 )
 
-    # convert qr to image.
-    png = qr.to_img
+    if options[:html] then
+      # since we have --html, print each QR code as an HTML table.
+      html_output << <<-eos
+        <h1>#{directory}</h1>
+        #{html_qrcode( qr )}
+        <hr />
+      eos
+    end
 
-    # print a table of what directory goes with what image.
-    puts "Share: #{directory} => #{filename}"
+    if options[:png] then
+      # convert qr to image.
+      png = qr.to_img
 
-    # save out the png file, resizing it.
-    png.resize( options[:size], options[:size] ).save( filename )
+      # print a table of what directory goes with what image.
+      puts "Share: #{directory} => #{filename}"
+
+      # save out the png file, resizing it.
+      png.resize( options[:size], options[:size] ).save( filename )
+    end
+
+    if options[:html] then
+      # close open html tags
+      html_output << <<-eos
+        </body>
+        </html>
+      eos
+
+      # write the output to the file specified
+      File.write( options[:html], html_output )
+    end
   end
+
 else
   # error message in case file does not exist.
   puts "File #{options[:file]} does not exist"
